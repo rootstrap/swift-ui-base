8639097c7ba718d9952ff8fac259275ff54b6818
German Stabile <german.stabile@gmail.com>
2020-03-10 14:18:42 -0300

    update gitignore, remove pods folder

--- Pods/Target Support Files/Pods-swift-ui-baseTests/Pods-swift-ui-baseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_ui_baseTests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_ui_baseTests
-@end
--- Pods/Target Support Files/Pods-swift-ui-baseUITests/Pods-swift-ui-baseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_ui_baseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_ui_baseUITests
-@end
