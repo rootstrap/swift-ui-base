63c8975c9d8fb4380d82c1ce29a859dd10365d51
German Stabile <german.stabile@gmail.com>
2020-06-18 14:22:59 -0300

    missing issue

--- swift-ui-base/AppDelegate.swift
+++ swift-ui-base/AppDelegate.swift
@@ -35,7 +35,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   ) -> UISceneConfiguration {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
   }
   
   func unexpectedLogout() {
