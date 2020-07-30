153277df00392b2b9b664caaaacfbf687f594718
Pablo Malvasio <p.a.malvasio@gmail.com>
2020-07-30 17:30:09 -0300

    Color assets + extension to call them + Rootstrap color palette

--- swift-ui-base/Extensions/ColorExtension.swift
+++ swift-ui-base/Extensions/ColorExtension.swift
@@ -7,27 +7,12 @@
 //
 
 import SwiftUI
-import Foundation
 
 extension Color {
   
-  static let darkGray = UIColor(red: 54, green: 61, blue: 79).asColor()
-  static let athensGray = UIColor(red: 229, green: 231, blue: 237).asColor()
-  static let altoGray = UIColor(red: 216, green: 216, blue: 216).asColor()
-  static let cadetBlue = UIColor(red: 167, green: 175, blue: 199).asColor()
-  static let lightGray = UIColor(red: 167, green: 175, blue: 199).asColor()
-  static let errorRed = UIColor(red: 255, green: 81, blue: 82).asColor()
-}
-
-extension UIColor {
-  convenience init(red: Int, green: Int, blue: Int) {
-    self.init(red: min(CGFloat(red), 255.0) / 255.0,
-              green: min(CGFloat(green), 255.0) / 255.0,
-              blue: min(CGFloat(blue), 255.0) / 255.0,
-              alpha: 1.0)
-  }
-  
-  func asColor() -> Color {
-    return Color(self)
-  }
+  static let darkGray = Color("darkGray")
+  static let athensGray = Color("athensGray")
+  static let altoGray = Color("altoGray")
+  static let cadetBlue = Color("cadetBlue")
+  static let errorRed = Color("errorRed")
 }
